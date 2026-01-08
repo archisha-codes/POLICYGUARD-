ff3db26cf9d139c8dc43c34aca95a39d25d0c743
Pawandubey11 <pawandubey6204385@gmail.com>
2026-01-08 21:45:39 +0530

    Update IBM Watson API URL to EU region

--- backend/agents/compliance_agent.py
+++ backend/agents/compliance_agent.py
@@ -21,7 +21,7 @@ def analyze_with_granite(transaction_data, retrieved_policies):
         params=GenerateParams,
         credentials={
             "apikey": WATSONX_API_KEY,
-            "url": "https://jp-tok.ml.cloud.ibm.com"
+            "url": "https://eu-de.ml.cloud.ibm.com
         },
         project_id=PROJECT_ID
     )
